72a3d8c9c7cf5cdeced52f2ebe63e2c9cb3faec1
CM <cristiandml3722@gmail.com>
2024-05-10 18:02:03 -0500

    Created the file-system datasource implementing the log datasource abstract class. Here the logic for writing the logs into a folder and into individual log files depending on severity and also the logic for retrieving the logs was created.
    Logic for:
    Creating log folder if it doesn't exists.
    Creating log file depending on severity level.
    Saving into each file depending on the severity level of the log passed to the instance.
    Getting the logs depending on the severity level.

---
+++ src/infrastructure/datasources/file-system.datasource.ts
@@ -0,0 +1,69 @@
+import {
+  appendFileSync,
+  existsSync,
+  mkdir,
+  mkdirSync,
+  readFileSync,
+  writeFileSync,
+} from "fs";
+import { LogDatasource } from "../../domain/datasources/log.datasource";
+import { LogEntity, LogSeverityLevel } from "../../domain/entities/log.entity";
+
+export class FileSystemDatasource implements LogDatasource {
+  private readonly logsPath = "./logs";
+  private readonly lowLogsPath = "./logs/logs-low.log";
+  private readonly mediumLogsPath = "./logs/logs-medium.log";
+  private readonly highLogsPath = "./logs/logs-high.log";
+
+  constructor() {
+    this.createLogsFiles();
+  }
+
+  private createLogsFiles = () => {
+    //Create logs folder if not exists
+    if (!existsSync(this.logsPath)) {
+      mkdirSync(this.logsPath);
+    }
+    //Create logs files if not exists
+    [this.lowLogsPath, this.mediumLogsPath, this.highLogsPath].forEach(
+      (path) => {
+        //If file exists, do nothnig else create it and write empty file
+        if (existsSync(path)) return;
+        writeFileSync(path, "");
+      }
+    );
+  };
+
+  public async saveLog(log: LogEntity): Promise<void> {
+    const logAsJson = `${JSON.stringify(log)}\n`;
+    //Save all logs to logs-low.log
+    appendFileSync(this.lowLogsPath, logAsJson);
+    //Save only medium logs to logs-medium.log
+    if (log.level === LogSeverityLevel.medium) {
+      appendFileSync(this.mediumLogsPath, logAsJson);
+    }
+    //Save only high logs to logs-high.log
+    if (log.level === LogSeverityLevel.high) {
+      appendFileSync(this.highLogsPath, logAsJson);
+    }
+  }
+
+  private getLogsFromFile = (path: string): LogEntity[] => {
+    const logs = readFileSync(path, "utf-8").toString().split("\n");
+    return logs.map((log) => LogEntity.fromJson(log));
+  };
+
+  public async getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
+    switch (severityLevel) {
+      case LogSeverityLevel.low:
+        return this.getLogsFromFile(this.lowLogsPath);
+      case LogSeverityLevel.medium:
+        return this.getLogsFromFile(this.mediumLogsPath);
+      case LogSeverityLevel.high:
+        return this.getLogsFromFile(this.highLogsPath);
+      default:
+        throw new Error(`Unknown severity level for logs: ${severityLevel}`);
+    }
+    return [];
+  }
+}
