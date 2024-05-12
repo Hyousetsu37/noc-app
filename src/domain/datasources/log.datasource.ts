f0e56b827c8cf071c4fe24a354bdb00593254df2
CM <cristiandml3722@gmail.com>
2024-05-12 00:15:13 -0500

    Comments added for better understanding

--- src/domain/datasources/log.datasource.ts
+++ src/domain/datasources/log.datasource.ts
@@ -1,5 +1,6 @@
 import { LogEntity, LogSeverityLevel } from "../entities/log.entity";
 
+//Stablish the rules so any object we want could be considered a Data Origin
 export abstract class LogDatasource {
   abstract saveLog(log: LogEntity): Promise<void>;
   abstract getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]>;
