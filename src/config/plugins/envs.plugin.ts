1ed45d84d870f156f47dfee25e94e968d1a46fc4
CM <cristiandml3722@gmail.com>
2024-05-12 00:10:09 -0500

    Installation and setup of dependencies to start using the data bases, MongoDB and PostgreSQL

--- src/config/plugins/envs.plugin.ts
+++ src/config/plugins/envs.plugin.ts
@@ -6,4 +6,10 @@ export const envs = {
   MAILER_EMAIL: env.get("MAILER_EMAIL").required().asEmailString(),
   MAILER_PASSWORD: env.get("MAILER_PASSWORD").required().asString(),
   MAILER_SERVICE: env.get("MAILER_SERVICE").required().asString(),
+
+  //MongoDB
+  MONGO_URL: env.get("MONGO_URL").required().asString(),
+  MONGO_DB_NAME: env.get("MONGO_DB_NAME").required().asString(),
+  MONGO_USER: env.get("MONGO_USER").required().asString(),
+  MONGO_PASS: env.get("MONGO_PASS").required().asString(),
 };
